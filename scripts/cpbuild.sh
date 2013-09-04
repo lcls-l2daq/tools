4f4f2b892aba9e690346e1ba56b01e83fa7493fa
weaver <none@example.com>
2013-09-04 17:24:45 +0000

    Remove copy of pdsdata header files from cpbuild script

--- scripts/cpbuild.sh
+++ scripts/cpbuild.sh
@@ -12,7 +12,6 @@ mkdir -p ${DAQREL}/ami
 rsync -rlpogtSP --exclude={dep,obj} build ${DAQREL}/.
 
 #  Copy the header files
-rsync -rlpogtSP --exclude={Makefile,CVS,src,\*.mk} pdsdata ${DAQREL}/.
 rsync -rlpogtSP --exclude={Makefile,CVS,\*.cc,\*.mk} ami/data ${DAQREL}/ami/.
 
 #  Copy header files from unwanted dependencies
