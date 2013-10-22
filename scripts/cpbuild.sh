97d1dd3c203320a525bae4dfd1bb1efdf87dcb06
weaver <none@example.com>
2013-10-22 00:40:10 +0000

    *** empty log message ***

--- scripts/cpbuild.sh
+++ scripts/cpbuild.sh
@@ -6,9 +6,12 @@ if ($# < 1) exit
 
 setenv DAQREL /reg/g/pcds/dist/pds/$1
 mkdir -p ${DAQREL}
-
 #  Copy libraries and binaries
 rsync -rlpogtSP --exclude={dep,obj} build ${DAQREL}/.
+
+#  Copy the header files
+mkdir -p ${DAQREL}/build/timetool/service
+rsync -rlpogtSP --exclude={Makefile,CVS,\*.cc,\*.mk} timetool/service ${DAQREL}/build/timetool/.
 
 #  Copy tools
 rsync -rlpogtSP --exclude={CVS} tools ${DAQREL}/.
