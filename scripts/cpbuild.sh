86dfad1460f8c0ae9afccd6665cbda3be3f3af44
weaver <none@example.com>
2013-10-22 00:14:32 +0000

    Remove ami from DAQ release copy

--- scripts/cpbuild.sh
+++ scripts/cpbuild.sh
@@ -6,17 +6,9 @@ if ($# < 1) exit
 
 setenv DAQREL /reg/g/pcds/dist/pds/$1
 mkdir -p ${DAQREL}
-mkdir -p ${DAQREL}/ami
 
 #  Copy libraries and binaries
 rsync -rlpogtSP --exclude={dep,obj} build ${DAQREL}/.
-
-#  Copy the header files
-rsync -rlpogtSP --exclude={Makefile,CVS,\*.cc,\*.mk} ami/data ${DAQREL}/ami/.
-
-#  Copy header files from unwanted dependencies
-mkdir -p ${DAQREL}/ami/event
-cp ami/event/CspadTemp.hh ${DAQREL}/ami/event/.
 
 #  Copy tools
 rsync -rlpogtSP --exclude={CVS} tools ${DAQREL}/.
