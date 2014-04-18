f38dd4ff1beaa09fd0adc785dce307f40ae472b5
weaver <none@example.com>
2014-04-18 19:30:14 +0000

    *** empty log message ***

--- scripts/cpbuild_ami.sh
+++ scripts/cpbuild_ami.sh
@@ -13,3 +13,5 @@ rsync -rlpogtSP --exclude={dep,obj} build ${AMIREL}/.
 
 #  Copy the header files
 rsync -rlpogtSP --exclude={Makefile,CVS,\*.cc,\*.mk} ami/data ${AMIREL}/ami/.
+mkdir -p ${AMIREL}/ami/event
+rsync -rlpogtSP ami/event/CspadTemp.hh ${AMIREL}/ami/event/.
