b7f8b6adf968c445cbffd1eea1e66a594ad32767
caf <caf@SLAC.STANFORD.EDU>
2015-02-05 01:36:40 +0000

    add sanity checks and support for -f flag to bypass them

--- scripts/tagrelease.py
+++ scripts/tagrelease.py
@@ -3,16 +3,36 @@
 
 import os
 import subprocess
+import sys
 from tempfile import NamedTemporaryFile, mkdtemp
 from shutil import rmtree
+from optparse import OptionParser
+
+usage = "Usage: %prog [options] tag"
+parser = OptionParser(usage=usage)
+
+parser.add_option("-f", "--force",
+                  action="store_true", dest="force", default=False,
+                  help="skip sanity checks (use with caution!)")
 
 tag=None
 DAQURL='https://pswww.slac.stanford.edu/svn/pdsrepo'
+
+daq_subdirs = ['pds', 'pdsapp', 'timetool', 'tools']
+ami_subdirs = ['ami', 'tools']
 
 def make_tag(pkg):
     path=DAQURL+'/'+pkg
     cmd = ['svn','copy',path+'/trunk',path+'/tags/'+tag,'-m','\"Tag '+tag+'\"']
     return subprocess.call(cmd)
+
+def verify_tag(pkg):
+    rv = False
+    p = subprocess.Popen(['/usr/bin/svn info '+DAQURL+'/'+pkg+'/tags/'+tag], shell = True,
+                         stdout = subprocess.PIPE, stderr = subprocess.PIPE, close_fds = True)
+    if subprocess.Popen.communicate(p)[0]:
+        rv = True
+    return rv
 
 def make_tagd(pkg):
     retval = 0
@@ -72,37 +92,68 @@ def update_externals_property(tmpdir, project, oldtext, newtext):
 
 if __name__ == '__main__':
 
-    import sys
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
+    else:
+        tag = args[0]
+
+    if tag.find('ami') >= 0:
+        dirlist = ami_subdirs + ['ami-release']
+    else:
+        dirlist = daq_subdirs + ['release']
+
+    if not options.force:
+        fail = False
+        # sanity check 1: tag begins with 'V' or 'ami-V'
+        if tag.find('V') and tag.find('ami-V'):
+            print 'sanity check failed: tag \'%s\' does not begin with \'V\' or \'ami-V\'' % tag 
+            fail = True
+
+        # sanity check 2: tag does not already exist
+        for dir in dirlist:
+            if verify_tag(dir):
+                print 'sanity check failed: tag \'%s\' already exists for directory \'%s\'' % (tag, dir)
+                fail = True
+
+        # sanity check 3: current directory has 'trunk' in svn:externals property
+        cwdprop = get_externals_property(".")
+        if (not cwdprop) or (cwdprop.find('trunk') == -1):
+            print 'sanity check failed: working directory does not have \'trunk\' in svn:externals property' 
+            fail = True
+
+        # sanity check 4: current directory contains expected subdirectories
+        for dir in dirlist:
+            if dir != 'release' and dir != 'ami-release' and not os.path.isdir(dir):
+                print 'sanity check failed: working directory does not include \'%s\' subdirectory' % dir
+                fail = True
+        if fail: 
+            print 'tag \'%s\' not applied due to sanity check failure' % tag
+            sys.exit(1)
 
     retval = 1
-    if len(sys.argv) != 2:
-        print 'Usage: '+sys.argv[0]+' <tag>'
-    else:
-        tag=sys.argv[1]
-        tmpdir = mkdtemp()  # create temporary directory
-        if tag.find('ami')<0:
-            if make_tag('release'):
-                print 'Error: tagging release failed'
-            else:
-                make_tagd('pds')
-                make_tagd('pdsapp')
-                make_tagd('timetool')
-                make_tagd('tools')
-                # update externals
-                if update_externals_property(tmpdir, 'release', 'trunk', 'tags/'+tag):
-                    print 'Error: updating svn:externals property failed'
-                else:
-                    retval = 0  # success
+    tmpdir = mkdtemp()      # create temporary directory
+    if tag.find('ami')<0:
+        if make_tag('release'):
+            print 'Error: tagging release failed'
         else:
-            if make_tag('ami-release'):
-                print 'Error: tagging ami-release failed'
+            for dir in daq_subdirs:
+                make_tagd(dir)
+            # update externals
+            if update_externals_property(tmpdir, 'release', 'trunk', 'tags/'+tag):
+                print 'Error: updating svn:externals property failed'
             else:
-                make_tagd('ami')
-                make_tagd('tools')
-                # update externals
-                if update_externals_property(tmpdir, 'ami-release', 'trunk', 'tags/'+tag):
-                    print 'Error: updating svn:externals property failed'
-                else:
-                    retval = 0  # success
-        rmtree(tmpdir)              # delete temorary directory
+                retval = 0  # success
+    else:
+        if make_tag('ami-release'):
+            print 'Error: tagging ami-release failed'
+        else:
+            for dir in ami_subdirs:
+                make_tagd(dir)
+            # update externals
+            if update_externals_property(tmpdir, 'ami-release', 'trunk', 'tags/'+tag):
+                print 'Error: updating svn:externals property failed'
+            else:
+                retval = 0  # success
+    rmtree(tmpdir)          # delete temorary directory
     sys.exit(retval)
