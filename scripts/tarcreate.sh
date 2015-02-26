95be841784c29af576d09077b3b7f2f451079599
caf <caf@SLAC.STANFORD.EDU>
2015-02-26 01:03:41 +0000

    set perms to 755 for tar files

--- scripts/tarcreate.sh
+++ scripts/tarcreate.sh
@@ -10,9 +10,18 @@ if [[ $# -ne 1 || ${1:0:1} == '-' ]]; then
   exit 1
 fi
 
+if [[ ! -d build ]] || [[ ! -d tools ]]; then
+  echo "Please cd to the directory above build and tools before running this script"
+  exit 1
+fi
+
 # copy the header files
 /bin/mkdir -pv build/timetool/service
 /bin/cp -pv timetool/service/*.hh build/timetool/service
+
+# set file and dir permissions
+/usr/bin/find build -type f -o -type d -exec /bin/chmod 755 {} \;
+/usr/bin/find tools -type f -o -type d -exec /bin/chmod 755 {} \;
 
 # create the tar file
 /bin/tar czvf $1 --exclude=.svn --exclude=dep --exclude=obj --exclude=.buildbot-sourcedata build tools
--- scripts/tarcreate_ami.sh
+++ scripts/tarcreate_ami.sh
@@ -10,6 +10,16 @@ if [[ $# -ne 1 || ${1:0:1} == '-' ]]; then
   exit 1
 fi
 
+if [[ ! -d build ]] || [[ ! -d ami ]] || [[ ! -d tools ]] ; then
+  echo "Please cd to the directory above build, ami, and tools before running this script"
+  exit 1
+fi
+
+# set file and dir permissions
+/usr/bin/find build -type f -o -type d -exec /bin/chmod 755 {} \;
+/usr/bin/find ami   -type f -o -type d -exec /bin/chmod 755 {} \;
+/usr/bin/find tools -type f -o -type d -exec /bin/chmod 755 {} \;
+
 # create the tar file
 /bin/tar czvf $1 \
   --exclude=.svn --exclude=dep --exclude=obj --exclude='*.cc' --exclude=.buildbot-sourcedata \
